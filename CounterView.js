83438b5d640dfacb85585a3ad744f77ca664e00b
YOUNGJOO-YOON <zerozoo-front@gmail.com>
2021-10-28 14:45:22 +0900

    remove comment

--- CounterView.js
+++ CounterView.js
@@ -1,6 +1,5 @@
 import React from "react"
 export default function CounterView({ value, onIncrease, onDecrease }) {
-  //   console.log("props : ", onIncrease, onDecrease, value)
   return (
     <>
       <button onClick={onIncrease}>+</button>
