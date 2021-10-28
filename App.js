583b40979350c2fcc43a9b2dff47c24476fc0392
YOUNGJOO-YOON <zerozoo-front@gmail.com>
2021-10-28 17:22:26 +0900

    can we use browserAPI in react component

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import Counter from "./Counter"
 
 export default function App() {
-  const [num, setNum] = useState(0)
+  const [favicon, setFavicon] = useState("")
+  const handlePreFetch = () => {
+    const request = new Request("https://www.mozilla.org/favicon.ico")
+    try {
+      fetch(request).then((res) => {
+        console.log("res: ", res)
+        setFavicon(res.url)
+        window.localStorage.setItem("url", res.url)
+      })
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  useEffect(() => {
+    handlePreFetch()
+  }, [])
   return (
     <>
       <h1>hello world!</h1>
       <h2>Server Side Rendering is running!</h2>
-      <h3>{num}</h3>
+      <img src={favicon} alt="favicon" />
       <Counter />
     </>
   )
